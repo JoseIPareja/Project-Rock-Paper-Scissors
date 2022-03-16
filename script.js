2c849f23f10c94d4c9362274b42ff02c5e66cf62
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-03-16 17:40:10 -0300

    First Steps

---
+++ script.js
@@ -0,0 +1,8 @@
+function computerPlay() {
+    let options = ["Rock", "Paper", "Scissors"];
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function playRund(playerSelection, computerSelection) {
+    //return a string that declares the wi 
+}
