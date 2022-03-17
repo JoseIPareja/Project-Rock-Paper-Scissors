a2a1114b335832c699a140a55660ad10c85f518d
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-03-17 00:43:02 -0300

    Finished JS prototype of the game

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
-function computerPlay() {
-    let options = ["Rock", "Paper", "Scissors"];
-    return options[Math.floor(Math.random() * options.length)];
-}
+function playRound(playerSelection, computerSelection) {  
+    if (playerSelection == 'rock') {
+      if (computerSelection == 'scissors') {
+        return 'win';
+      } else if (computerSelection == 'paper') {
+        return 'loss';
+      } else {
+        return 'tie';
+      } 
+    }
+    if (playerSelection == 'paper') {
+      if (computerSelection == 'rock') {
+        return 'win';
+      } else if (computerSelection == 'scissors') {
+        return 'losss';
+      } else {
+        return 'tie';
+      } 
+    }
+    if (playerSelection == 'scissors') {
+      if (computerSelection == 'rock') {
+        return 'loss';
+      } else if (computerSelection == 'paper') {
+        return 'win';
+      } else {
+        return 'tie';
+      } 
+    }
+  }
 
-function playRund(playerSelection, computerSelection) {
-    //return a string that declares the wi 
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    const games = 5;
+    
+    for (let i = 0; i < games; i++) {
+        let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+        function computerPlay() { return (["paper","scissors","rock"])[Math.random() * 3 | 0]; }
+        let computerSelection = computerPlay();
+
+        let round = playRound(playerSelection, computerSelection);
+        if (round === "tie") {
+            console.log("It's a tie");
+        } else if (round === "win") {
+            console.log("Player wins");
+            playerScore++;
+        } else if (round === "loss") {
+            console.log("Computer wins");
+            computerScore++;
+        }
+    }
+    if (playerScore > computerScore) {
+        console.log("YAY, You are the winner!!");
+    } else {
+        console.log("GAME OVER")
+    }
 }
+console.log(game())
